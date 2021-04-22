b64c04e7d3363e55e5a4063f457eec767ae0b531
Cybercat-jpg <69266449+Cybercat-jpg@users.noreply.github.com>
2021-04-22 16:38:27 +0200

    Update Filter_Reads_by_length.py

--- Filter_Reads_by_length.py
+++ Filter_Reads_by_length.py
@@ -3,19 +3,23 @@
 ### Simple script for dropping sequences below a specific length
 ### I just created this script to drop reads below a specific length from my ONT data.
 
+### Still not working how I want it.
+
 from Bio import SeqIO
 
-input_file = "/home/yvonne/Desktop/ls_orchid.fasta"
-output_file = "/home/yvonne/Desktop/filtered.faa"
+input_file = '/home/yvonne/Documents/HaliclonaReads/ONT.fasta'
+output_file = open('/home/yvonne/Documents/HaliclonaReads/Filtered/filtered.faa', 'w')
 
 long_sequences = []
-dropped = []
+#dropped = []
 for seq_record in SeqIO.parse(input_file, "fasta"):
     print(len(seq_record.seq))
     if len(seq_record.seq) >= 300:
-        long_sequences.append(seq_record)
-    else:
-        dropped.append(seq_record)
+        
+        output_file.write(seq_record.seq)
+    #else:
+        #dropped.append(seq_record)
 
-SeqIO.write(long_sequences, output_file, "fasta")
-SeqIO.write(dropped, "/home/yvonne/Desktop/dropped.faa", "fasta")
+output_file.close()
+#SeqIO.write(long_sequences, output_file, "fasta")
+#SeqIO.write(dropped, "/home/yvonne/Desktop/dropped.faa", "fasta")
